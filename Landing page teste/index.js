a130a3e9d69d726e10959e41e44ba843a8328bd0
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-03-27 12:27:31 -0300

    ...

---
+++ Landing page teste/index.js
@@ -0,0 +1,19 @@
+let menuclose = document.querySelector('#menuclose')
+let lista = document.querySelector('#ulmenu')
+let menumobile = document.querySelector('.menu_mobile')
+let menustop = document.querySelector('#menustop')
+
+function menufechado() {
+    lista.style.display = 'block'
+    menumobile.style.background = '#244b55'
+    menuclose.style.display = 'none'
+    menustop.style.display = 'block'
+}
+
+function fecharmenu() {
+    lista.style.display = 'none'
+    menumobile.style.background = '#fff'
+    menuclose.style.display = 'block'
+    menustop.style.display = 'none'
+    menustop.style.background = '#fff'
+}
