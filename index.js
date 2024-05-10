527ebfc8901c0f982b2cfe234cb9a8d4221518ba
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-05-10 16:38:21 -0300

    Atualizações do site.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ let menuclose = document.querySelector('#menuclose')
 let lista = document.querySelector('#ulmenu')
 let menumobile = document.querySelector('.menu_mobile')
 let menustop = document.querySelector('#menustop')
+let modal = document.querySelector('.modal')
+let texto = document.querySelector('.texto')
+let texto1 = document.querySelector('.texto_1')
+let texto2 = document.querySelector('.texto_2')
+let proximo = document.querySelector('.proximo')
+let fechar = document.querySelector('.fechar')
+let conferir = document.querySelector('.conferir')
+let som = document.querySelector('.som')
+som.volume = 0.1;
 
 function menufechado() {
     lista.style.display = 'block'
@@ -17,4 +26,28 @@ function fecharmenu() {
     menustop.style.display = 'none'
     menustop.style.background = '#fff'
 }
+
+fechar.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+
+proximo.addEventListener('click', () => {
+    texto.style.display = 'none'
+    texto1.style.display = 'block'
+    texto2.style.display = 'block'
+    conferir.style.display = 'block'
+    proximo.style.display = 'none'
+})
+
+conferir.addEventListener('click', () => {
+    modal.style.display = 'none'
+    som.play()
+})
+
+
+
+
+
+
+
 
