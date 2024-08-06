7b084bf18b90fb9cdcd2b128983f64533fc49b7c
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-08-06 14:36:19 -0300

    Implementando outros testes.

---
+++ api-cosmeticos/testes/index.test.js
@@ -0,0 +1,25 @@
+const request = require("supertest");
+const express = require("express");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+
+app.use(express.json());
+
+app.use(cors());
+let produtosRouter = require("../rotas/product");
+const connectDB = require("./db");
+const { error } = require("console");
+
+connectDB();
+
+app.use("/product", produtosRouter);
+
+app.get("/", (req, res) => {
+  res.send(produtosRouter);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em ${port}`);
+});
